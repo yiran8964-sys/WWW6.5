6101a38bf6aaeb028605d810d364568a7b71020a
yiran8964-sys <yiran8964@gmail.com>
2026-03-19 21:42:10 +0800

    处理chainlink版本更新导致无法引用到相应sol文件

--- yiran8964/day18_CropInsurance.sol
+++ yiran8964/day18_CropInsurance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CropInsurance is Ownable {
