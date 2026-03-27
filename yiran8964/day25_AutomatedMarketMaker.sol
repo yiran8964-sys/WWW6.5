69efbaacc5bfe77c7d46e11c1f841306ccd00fd4
yiran8964-sys <yiran8964@gmail.com>
2026-03-27 20:53:13 +0800

    提交第二十五天作业

---
+++ yiran8964/day25_AutomatedMarketMaker.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AutomatedMarketMaker is ERC20 {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    uint256 public reserveA;
+    uint256 public reserveB;
+
+    address public owner;
+
+    event LiquidityAdded(address indexed provider, uint256 amountA, uint256 amountB, uint256 liquidity);
+    event LiquidityRemoved(address indexed provider, uint256 amountA, uint256 amountB, uint256 liquidity);
+    event TokensSwapped(address indexed trader, address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut);
+
+    constructor(address _tokenA, address _tokenB, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        owner = msg.sender;
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be > 0");
+
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        uint256 liquidity;
+        if (totalSupply() == 0) {
+            liquidity = sqrt(amountA*amountB);
+        } else {
+            liquidity = min(amountA*totalSupply()/reserveA, amountB*totalSupply()/reserveB);
+        }
+
+        _mint(msg.sender, liquidity);
+
+        reserveA += amountA;
+        reserveB += amountB;
+        emit LiquidityAdded(msg.sender, amountA, amountB, liquidity);
+    }
+
+    function removeLiquidity(uint256 liquidityToRemove) external returns(uint256 amountAOut, uint256 amountBOut) {
+        require(liquidityToRemove > 0, "Liquidity to remove must be > 0");
+        require(balanceOf(msg.sender) >= liquidityToRemove, "Insufficient liquidity tokens");
+
+        uint256 totalLiquidity = totalSupply();
+        require(totalLiquidity > 0, "No liquidity in the pool");
+
+        amountAOut = liquidityToRemove * reserveA / totalLiquidity;
+        amountBOut = liquidityToRemove * reserveB / totalLiquidity;
+
+        require(amountAOut > 0 && amountBOut > 0, "Insufficient reserves for requested liquidity");
+
+        reserveA -= amountAOut;
+        reserveB -= amountBOut;
+
+        _burn(msg.sender, liquidityToRemove);
+
+        tokenA.transfer(msg.sender, amountAOut);
+        tokenB.transfer(msg.sender, amountBOut);
+
+        emit LiquidityRemoved(msg.sender, amountAOut, amountBOut, liquidityToRemove);
+
+        return(amountAOut, amountBOut);
+    }
+
+    function swapAforB(uint256 amountAIn, uint256 minBOut) external {
+        require(amountAIn > 0, "Amount must be > 0");
+        require(reserveA > 0 && reserveB > 0, "Insufficient reserves");
+
+        uint256 amountAInWithFee = amountAIn * 997 / 1000;
+        uint256 amountBOut = reserveB * amountAInWithFee / (reserveA + amountAInWithFee);
+
+        require(amountBOut >= minBOut, "Slippage too high");
+
+        tokenA.transferFrom(msg.sender, address(this), amountAIn);
+        tokenB.transfer(msg.sender, amountBOut);
+
+        reserveA += amountAIn;
+        reserveB -= amountBOut;
+        emit TokensSwapped(msg.sender, address(tokenA), amountAIn, address(tokenB), amountBOut);
+    }
+
+    function swapBforA(uint256 amountBIn, uint256 minAOut) external {
+        require(amountBIn > 0, "Amount must be > 0");
+        require(reserveA > 0 && reserveB > 0, "Insufficient reserves");
+
+        uint256 amountBInWithFee = amountBIn * 997 / 1000;
+        uint256 amountAOut = reserveA * amountBInWithFee/ (reserveB + amountBInWithFee);
+
+        require(amountAOut >= minAOut, "Slippage too high");
+
+        tokenB.transferFrom(msg.sender, address(this), amountBIn);
+        tokenA.transfer(msg.sender, amountAOut);
+
+        reserveA -= amountAOut;
+        reserveB += amountBIn;
+
+        emit TokensSwapped(msg.sender, address(tokenB), amountBIn, address(tokenA), amountAOut);
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns(uint256) {
+        return a<b?a:b;
+    }
+
+    function sqrt(uint256 y) internal pure returns(uint256 z) {
+        if(y>3) {
+            z = y;
+            uint256 x = y/2 + 1;
+            while(x < z) {
+                z = x;
+                x = (y/x + x) / 2;
+            }
+        } else if(y != 0) {
+            z = 1;
+        }
+    }
+}
